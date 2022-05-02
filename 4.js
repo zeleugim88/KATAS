db6e4f8ce5534057204367794228795653eb34c2
DANI MI <zeleugim@gmail.com>
2022-05-02 21:10:23 +0200

    kata4-inicio

---
+++ 4.js
@@ -0,0 +1,17 @@
+//Implement a simple version of autocomplete, wehre given an input string "s" and a dictionary of words "dict",
+//return the word(s) in "dict" that partially match "s" (or an empty string if nothing matches).
+
+//Example:
+
+dict = ['apple', 'banana', 'cranberry','strawberry']
+
+//simpleAutocomplete('app')
+//['apple']
+
+//simpleAutocomplete('berry')
+//['cranberry','strawberry']
+
+//simpleAutocomplete('fart')
+//[]
+
+const simpleAutocomplete = (dict) => 
